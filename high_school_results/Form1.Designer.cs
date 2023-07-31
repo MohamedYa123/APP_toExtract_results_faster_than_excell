f2d53da8bb00539597b06f9480b0bf7cad2703df
MoahmedYa123 <tecyouth123@.com>
2023-07-31 23:40:18 +0300

    r

---
+++ high_school_results/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace high_school_results
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.stdname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stdnum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(493, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "show result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stdname
+            // 
+            this.stdname.Location = new System.Drawing.Point(339, 75);
+            this.stdname.Name = "stdname";
+            this.stdname.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.stdname.Size = new System.Drawing.Size(693, 22);
+            this.stdname.TabIndex = 1;
+            this.stdname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.stdname_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student name (slow search)";
+            // 
+            // stdnum
+            // 
+            this.stdnum.Location = new System.Drawing.Point(339, 31);
+            this.stdnum.Name = "stdnum";
+            this.stdnum.Size = new System.Drawing.Size(693, 22);
+            this.stdnum.TabIndex = 1;
+            this.stdnum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.stdname_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "student seat number";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.stdnum);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.stdname);
+            this.panel1.Location = new System.Drawing.Point(23, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1122, 188);
+            this.panel1.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 217);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1121, 540);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "index";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "seat number";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "student name";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "degree";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "percentage";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "student case";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 763);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1122, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(742, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "next 1000 students";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 798);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "high school degrees";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox stdname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox stdnum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ high_school_results/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace high_school_results
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (stdname.Text == "")
+            {
+                if (stdnum.Text == "")
+                {
+                    MessageBox.Show("Please enter student number !");
+                    return;
+                }
+                var stn=Convert.ToInt32(stdnum.Text);
+                var x=  seatnumss.IndexOf(stn);
+                if (x == -1)
+                {
+                    MessageBox.Show("Student number not found !");
+                    return;
+                }
+                list.Clear();
+                ListViewItem lv = new ListViewItem();
+                lv.Text = "1";
+                lv.SubItems.Add(stn.ToString());
+                lv.SubItems.Add(names2[x]);
+                lv.SubItems.Add(Math.Round(degrees[x],2)+"");
+                lv.SubItems.Add(Math.Round(degrees[x]/410*100,2)+" %");
+                lv.SubItems.Add(stcase[x]);
+                list.Add(lv);
+            }
+            else
+            {
+                string s = stdname.Text;
+                s =fine(s);
+                await Task.Run(() => {
+                    int nums = 0;
+                    List<int> ns = new List<int>();
+                    for(int i=0;i<names.Count;i++)
+                    {
+                        var zx = names[i];
+                        if (zx.Contains(s))
+                        {
+                            ns.Add(i);
+                            nums++; 
+                            if(nums>=maxnums)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    list.Clear();
+                    for(int i=0;i<ns.Count;i++)
+                    {
+                        var x = ns[i];
+                        ListViewItem lv = new ListViewItem();
+                        lv.Text = (i+1)+"";
+                        lv.SubItems.Add(seatnumss[x]+"");
+                        lv.SubItems.Add(names2[x]);
+                        lv.SubItems.Add(Math.Round(degrees[x], 2) + "");
+                        lv.SubItems.Add(Math.Round(degrees[x] / 410 * 100,2) + " %");
+                        lv.SubItems.Add(stcase[x]);
+                        list.Add(lv);
+                    }
+                });
+
+            }
+
+            listView1.Items.Clear();
+            listView1.Items.AddRange(list.ToArray());
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+        int maxnums = 5000;
+        List<ListViewItem> list = new List<ListViewItem>();
+        List<string> names = new List<string>();
+        List<string> names2 = new List<string>();
+        List<string> stcase = new List<string>();
+        List<int> seatnumss = new List<int>();
+        List<double> degrees = new List<double>();
+        static string path = "high_school_2023.csv";
+        int progress = 1;
+        int total;
+        string fine(string s)
+        {
+            return s.Replace("أ", "ا").Replace("إ", "ا").Replace("ة", "ه").Replace("ى", "ي").Replace(" ","").Replace("س","ص").Replace("ئ","ى").Replace("ء","ا");
+        }
+        async Task read()
+        {
+            await Task.Run(() => {
+                var f = File.ReadAllLines(path);
+                total = f.Length;
+                for (int i = 1; i < f.Length; i++)
+                {
+                    
+                    var x = f[i].Split(',');
+                    if (Convert.ToDouble(x[2]) <= 410)
+                    {
+                        names.Add(fine(x[1]));
+                        names2.Add(x[1]);
+                        seatnumss.Add(Convert.ToInt32(x[0]));
+                        degrees.Add(Convert.ToDouble(x[2]));
+                        stcase.Add(x[5]);
+                        
+                    }
+                    progress++;
+                }
+            });
+        }
+        void  search(string name)
+        {
+            list.Clear();
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+          
+            await read();
+        }
+        int yy = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (total > 0)
+            {
+                progressBar1.Value = (progress * 100) / total;
+                if (progressBar1.Value == 100)
+                {
+                    yy++;
+                
+                }
+            }
+            if (yy == 50)
+            {
+                  progressBar1.Hide();
+            }
+        }
+
+        private void stdname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (stdname.Text == "")
+            {
+                if (stdnum.Text == "")
+                {
+                    MessageBox.Show("Please enter student number !");
+                    return;
+                }
+                var stn = Convert.ToInt32(stdnum.Text);
+                var x = seatnumss.IndexOf(stn);
+                if (x == -1)
+                {
+                    MessageBox.Show("Student number not found !");
+                    return;
+                }
+                list.Clear();
+                for (int i = 0; i < 1000; i++)
+                {
+                    
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = (i+1)+"";
+                    lv.SubItems.Add(seatnumss[x] + "");
+                    lv.SubItems.Add(names2[x]);
+                    lv.SubItems.Add(Math.Round(degrees[x], 2) + "");
+                    lv.SubItems.Add(Math.Round(degrees[x] / 410 * 100, 2) + " %");
+                    lv.SubItems.Add(stcase[x]);
+                    list.Add(lv);
+                    x++;
+                }
+            }
+            listView1.Items.Clear();
+            listView1.Items.AddRange(list.ToArray());
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+    }
+}
